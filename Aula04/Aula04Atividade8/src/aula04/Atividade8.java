dd215c74cad8d9169408cdad5256167ee8a8cdb9
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 15:32:41 -0300

    Atividade8

---
+++ Aula04/Aula04Atividade8/src/aula04/Atividade8.java
@@ -0,0 +1,30 @@
+package aula04;
+import java.util.Locale;
+import java.util.Scanner;
+public class Atividade8 {
+    public static void main(String[] args) {
+        double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite o valor de A: ");
+        A = entrada.nextDouble();
+        System.out.println("Digite o valor de B: ");
+        B = entrada.nextDouble();
+        System.out.println("Digite o valor de C: ");
+        C = entrada.nextDouble();
+
+        triangulo = A * C / 2.0;
+        circulo = 3.14159 * C * C;
+        trapezio = (A + B) / 2.0 * C;
+        quadrado = B * B;
+        retangulo = A * B;
+
+        System.out.printf("TRIANGULO: %.3f%n", triangulo);
+        System.out.printf("CIRCULO: %.3f%n", circulo);
+        System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+        System.out.printf("QUADRADO: %.3f%n", quadrado);
+        System.out.printf("RETANGULO: %.3f%n", retangulo);
+        entrada.close();
+    }
+}
