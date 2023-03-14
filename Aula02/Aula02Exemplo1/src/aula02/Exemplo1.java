e5bce437df232a6f92cf26c2842804f60fcdaa5d
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 14:50:05 -0300

    Exemplo1

---
+++ Aula02/Aula02Exemplo1/src/aula02/Exemplo1.java
@@ -0,0 +1,10 @@
+package aula02;
+import java.util.Date;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Date relogio = new Date();  
+        System.out.println("A hora do sistema e:");
+        System.out.println(relogio.toString());  
+    }    
+}
