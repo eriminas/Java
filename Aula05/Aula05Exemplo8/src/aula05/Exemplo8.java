002cedf1f5d671837faaae1036032dc41bf9e16f
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:53:02 -0300

    Exemplo8

---
+++ Aula05/Aula05Exemplo8/src/aula05/Exemplo8.java
@@ -0,0 +1,13 @@
+package aula05;
+public class Exemplo8 {
+    public static void main(String[] args) {
+        int soma = 1;
+        for (int i = 1; i < 10; i++) {            
+            soma = soma * i;
+            System.out.println("O fatorial de:" + i + "=" + soma);            
+        }
+    }    
+}
+
+
+
