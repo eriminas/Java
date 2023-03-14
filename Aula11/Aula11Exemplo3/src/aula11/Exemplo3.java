d1a1707fdce7b0fc79a6420dec2515ccfc0ae0af
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 18:03:38 -0300

    Exemplo3

---
+++ Aula11/Aula11Exemplo3/src/aula11/Exemplo3.java
@@ -0,0 +1,19 @@
+package aula11;
+import java.util.Scanner;
+public class Exemplo3 {
+    public static void main(String[] args) {
+        int i;
+        int num[] = new int[4];
+        Scanner entrada = new Scanner(System.in);        
+        for (i=0; i<4; i++) {
+            System.out.println("Digite o numero da posicao " + i + " :");
+            num[i] = entrada.nextInt();                   
+        }        
+        int total = num[0] + num[1] + num[2] + num[3];
+        System.out.println("A soma dos elementos do vetor eh: " + total);
+    }
+}
+
+
+
+
