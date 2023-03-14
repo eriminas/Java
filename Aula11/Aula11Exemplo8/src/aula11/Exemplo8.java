1aececa44f9ab974b848784e577fb7bc9aba72e2
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 18:07:36 -0300

    Exemplo8

---
+++ Aula11/Aula11Exemplo8/src/aula11/Exemplo8.java
@@ -0,0 +1,11 @@
+package aula11;
+public class Exemplo8 {
+    public static void main(String[] args) {
+         // usando o método max() da classe Math  
+      System.out.print ( "O maximo eh:"  + Math.max(13, 7));  
+    }    
+}
+
+
+
+
