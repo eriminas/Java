e68855e0703181ab1c1c921c880baf35a45ca757
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 15:23:43 -0300

    Exemplo1

---
+++ Aula03/Aula03Exemplo1/src/aula03/Exemplo1.java
@@ -0,0 +1,20 @@
+package aula03;
+public class Exemplo1 {
+    public static void main(String[] args) {        
+        
+        String nome = "Jose";
+        int idade = 35;
+        double altura = 1.74;
+        char sexo = 'M';
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Sexo: " + sexo);        
+    }    
+}
+
+
+
+
+
