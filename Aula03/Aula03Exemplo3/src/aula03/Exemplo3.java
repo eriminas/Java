5addce995bdd5bc910c291c9bd24eb37ba0b0f06
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 15:24:56 -0300

    Exemplo3

---
+++ Aula03/Aula03Exemplo3/src/aula03/Exemplo3.java
@@ -0,0 +1,15 @@
+package aula03;
+public class Exemplo3 {
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        int idade = 42;
+        double renda = 8000.0;
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+    }
+}
+
+
+
+
+
