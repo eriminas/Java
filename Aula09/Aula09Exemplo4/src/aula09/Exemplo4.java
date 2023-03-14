4d4a597234e4382254ce97880b457c88f230e5cd
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 17:46:49 -0300

    Exemplo4

---
+++ Aula09/Aula09Exemplo4/src/aula09/Exemplo4.java
@@ -0,0 +1,12 @@
+package aula09;
+public class Exemplo4 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 100; i = i+2) {
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
+
+
+
