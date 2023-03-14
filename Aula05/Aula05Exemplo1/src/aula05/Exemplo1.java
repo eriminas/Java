1387a3a54b5743ae9e7db877951c4cae5f8f140a
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:33:02 -0300

    Exemplo1

---
+++ Aula05/Aula05Exemplo1/src/aula05/Exemplo1.java
@@ -0,0 +1,17 @@
+package aula05;
+public class Exemplo1 {
+    public static void main(String[] args) {
+        double A, B, C;
+        double x = 9.0;
+        double y = 4.0;
+        double z = -8.0;
+        
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(z);
+        
+        System.out.println("A raiz quadrada de " + x + " = " + A);
+        System.out.println("A raiz quadrada de " + y + " = " + B);
+        System.out.println("A raiz quadrada de " + z + " = " + C);                
+    }    
+}
