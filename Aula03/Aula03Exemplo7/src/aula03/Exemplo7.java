4a418030041284f7e7ed7c0cea68443d35393fc8
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 15:26:21 -0300

    Exemplo7

---
+++ Aula03/Aula03Exemplo7/src/aula03/Exemplo7.java
@@ -0,0 +1,16 @@
+package aula03;
+public class Exemplo7 {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;        
+        
+        a = 5;
+        b = 2;
+        resultado = (double)a/b;        
+        
+        System.out.println(resultado);
+    }
+}
+
+
+
