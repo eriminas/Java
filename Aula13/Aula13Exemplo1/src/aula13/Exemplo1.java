65886e85ee71ae4a1d71c6a0a18f6ff0b486015a
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:13:23 -0300

    Exemplo1

---
+++ Aula13/Aula13Exemplo1/src/aula13/Exemplo1.java
@@ -0,0 +1,35 @@
+package aula13;
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();          
+        c1.cor = "Azul";
+        c1.modelo = "Bic";
+        c1.carga = 90;
+        c1.ponta = 0.7f;
+        c1.tampada = false;
+        c1.rabiscar();
+        c1.status();
+        System.out.println(" ");        
+        
+        Caneta c2 = new Caneta();
+        c2.cor = "Vermelho";
+        c2.modelo = "Bic";
+        c2.carga = 100;
+        c2.ponta = 0.5f;
+        c2.tampada = true; 
+        c2.rabiscar();
+        c2.status();
+        System.out.println(" ");
+        
+        Caneta c3 = new Caneta();
+        c3.cor = "Preto";
+        c3.modelo = "Bic";
+        c3.ponta = 0.3f;
+        c3.status();
+    }
+}
+
+
+
+
+
