b189200c95779fcd6cb7aa0a9439e9bf0c107359
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 18:06:55 -0300

    Exemplo7

---
+++ Aula11/Aula11Exemplo7/src/aula11/Exemplo7.java
@@ -0,0 +1,34 @@
+package aula11;
+import java.util.Scanner;
+public class Exemplo7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Enter com 3 numeros:");
+        int a = entrada.nextInt();
+        int b = entrada.nextInt();
+        int c = entrada.nextInt();
+        int maior = max(a, b, c);
+        mostrarResultado(maior);
+        entrada.close();
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void mostrarResultado(int value) {
+        System.out.println("Maior = " + value);
+    }
+}
+
+
+
+
