25ebf0b26542bff496d3b56715ad4b3b434fdf0d
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 15:24:17 -0300

    Exemplo2

---
+++ Aula03/Aula03Exemplo2/src/aula03/Exemplo2.java
@@ -0,0 +1,16 @@
+package aula03;
+
+public class Exemplo2 {
+    public static void main(String[] args) {        
+        int y = 20;
+        double x = 21.54784;        
+        
+        System.out.print(y);
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);        
+    }
+}
+
+
+
