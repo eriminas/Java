d1bd327ca43ab22ef3ccfa859fc3fc4ac76c0d84
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:51:37 -0300

    Exemplo6

---
+++ Aula05/Aula05Exemplo6/src/aula05/Exemplo6.java
@@ -0,0 +1,11 @@
+package aula05;
+public class Exemplo6 {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero++;
+        numero--;
+        System.out.println("numero: " + numero);
+    }
+}
+
+
