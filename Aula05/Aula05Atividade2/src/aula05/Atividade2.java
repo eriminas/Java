dd224a73bf637be5529fa19112a0c62dda3eaad6
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:28:35 -0300

    Atividade2

---
+++ Aula05/Aula05Atividade2/src/aula05/Atividade2.java
@@ -0,0 +1,13 @@
+package aula05;
+public class Atividade2 {
+    public static void main(String[] args) {
+        System.out.println("10 == 10 && 10 == 20 --> " + (10 == 10 && 10 == 20));
+        System.out.println("11 == 12 && 20 == 20 --> " + (11 == 12 && 20 == 20));
+        System.out.println("15 == 15 && 16 == 16 --> " + (15 == 15 && 16 == 16));
+        System.out.println("12 == 11 && 23 == 24 --> " + (12 == 11 && 23 == 24));
+        System.out.println("10 == 10 || 10 == 20 --> " + (10 == 10 || 10 == 20));
+        System.out.println("11 == 12 || 20 == 20 --> " + (11 == 12 || 20 == 20));
+        System.out.println("15 == 15 || 16 == 16 --> " + (15 == 15 || 16 == 16));
+        System.out.println("12 == 11 || 23 == 24 --> " + (12 == 11 || 23 == 24));             
+    }    
+}
