a3280f09eb93461fa38eff0994bfd0b19b22f5e3
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 18:02:12 -0300

    Exemplo1

---
+++ Aula11/Aula11Exemplo1/src/aula11/Exemplo1.java
@@ -0,0 +1,15 @@
+package aula11;
+import java.util.Scanner;
+public class Exemplo1 {
+    public static void main(String[] args) {
+        String nome1, nome2, nome499, nome500;
+        
+        Scanner entrada = new Scanner(System.in);
+        nome1 = entrada.nextLine();
+        nome2 = entrada.nextLine();
+        nome499 = entrada.nextLine();
+        nome500 = entrada.nextLine();        
+    }    
+}
+
+
