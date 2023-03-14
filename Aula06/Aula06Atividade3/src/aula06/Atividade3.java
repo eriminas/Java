b98b5bc0a8079271766a9e9ed409dbdd7b3b8527
eriminas <43802210+eriminas@users.noreply.github.com>
2023-03-14 16:56:33 -0300

    Atividade3

---
+++ Aula06/Aula06Atividade3/src/aula06/Atividade3.java
@@ -0,0 +1,13 @@
+package aula06;
+import java.util.Scanner;
+
+public class Atividade3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //int a = 5;
+        float a = 5f;
+        double b = 3.0;
+        a /= b;
+        System.out.println("O resuldato final é: " + a);
+    }
+}
