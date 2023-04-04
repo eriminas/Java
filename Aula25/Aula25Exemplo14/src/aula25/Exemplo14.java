f20e75aa275400b369ec55ab0064820f539638c8
eriminas <43802210+eriminas@users.noreply.github.com>
2023-04-04 09:58:40 -0300

    Exemplo 14

---
+++ Aula25/Aula25Exemplo14/src/aula25/Exemplo14.java
@@ -0,0 +1,25 @@
+package aula25;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Exemplo14 {
+    public static void main(String[] args) {
+        List<Produto> list = new ArrayList<>();
+        list.add(new Produto("Tv", 900.00));
+        list.add(new Produto("Mouse", 50.00));
+        list.add(new Produto("Tablet", 350.50));
+        list.add(new Produto("HD Case", 80.90));           
+        
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase())
+                .collect(Collectors.toList());
+                names.forEach(System.out::println); 
+    }    
+}
+
+
+
+
+
+
