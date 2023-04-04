c5739e126b240b7a668b2e901771505793abf5fb
eriminas <43802210+eriminas@users.noreply.github.com>
2023-04-04 10:37:14 -0300

    Exemplo 2

---
+++ Aula18/Aula18Exemplo2/src/aula18/Exemplo2.java
@@ -0,0 +1,20 @@
+package aula18;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Posicao Invalida!");
+        } catch (InputMismatchException e) {
+            System.out.println("Erro de Entrada");
+        }
+        System.out.println("Fim odo programa");
+        sc.close();
+    }
+}
